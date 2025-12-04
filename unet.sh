4872a0d265fbaf74410253ac8c1b18cf2a4034b9
datepalm2 <syed.alam@kaust.edu.sa>
2025-12-04 13:21:21 +0300

    adding bash for unet

---
+++ unet.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Set ROS domain ID
+export ROS_DOMAIN_ID=5
+echo "ROS_DOMAIN_ID set to 5"
+
+# Flush ENO1 IPs
+echo "Flushing IP addresses on eno1..."
+sudo ip addr flush dev eno1
+
+# Set primary static IP
+echo "Adding 192.168.1.101/24 to eno1..."
+sudo ip addr add 192.168.1.101/24 dev eno1
+
+# Set secondary link-local IP
+echo "Adding 169.254.186.100/24 to eno1..."
+sudo ip addr add 169.254.186.100/24 dev eno1
+
+# Bring interface up
+echo "Bringing eno1 up..."
+sudo ip link set eno1 up
+
+echo "eno1 configuration complete."
+
